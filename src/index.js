0e938578bdcc6cf90fa0f1b55675935a9b1e4204
ViacheslavAnisimov <forvsn@gmail.com>
2023-07-31 07:15:37 +0700

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// module.exports = function reverse (n) {
+//   return String(Math.abs(n)).split('').reverse().join('');
+// }
+
 module.exports = function reverse (n) {
-  
+    let str = String(Math.abs(n)).split('');
+    let res = [];
+    for (let i = str.length; i >= 0; i--) {
+        res.push(str[i]);
+    }
+    return res.join('');
 }
